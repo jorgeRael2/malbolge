3b670c92f6824efafa4a8516b60d814f3010fc90
jorgeRael2 <168679666+jorgeRael2@users.noreply.github.com>
2024-09-02 01:08:42 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,21 @@
+//Developer: Jorge Rael Fortes de Faria Lopes
+
+//This is a malbolge interpreter.
+//This will work with files.
+//The source code should be stored in a file.
+//The input should be stored in a file.
+//The source code should have the name "code.mal"
+//The input should have the name "input.bin"
+//This program should work as same as original.
+//This source code should be easier to understand
+
+#include <stdio.h>
+#include <stdint.h>
+
+typedef struct{
+	uint8_t value;
+} ternaryDigit;
+
+int main(){
+	return 0;
+}
